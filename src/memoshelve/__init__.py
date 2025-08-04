8880cf264059c9bd6112a94bc4f83c7a078fadf2
Automated Publisher <actions@users.noreply.github.com>
2025-08-04 19:35:04 +0000

    Automated Package Version Bump: Mon Aug  4 19:35:04 UTC 2025 d5bc7f9c91052eefd0c43d4e51e689ae809d74c9

--- src/memoshelve/__init__.py
+++ src/memoshelve/__init__.py
@@ -47,7 +47,7 @@ def hash_via_stablehash(obj: object) -> str:
     return stablehash.stablehash(obj).hexdigest()
 
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 memoshelve_cache: dict[str, dict[str, Any]] = {}
 T = TypeVar("T")
